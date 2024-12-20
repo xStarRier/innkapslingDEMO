6b9a36715f58fa84e57c8f73f4a222931ab7d8ee
xStarRier <stephanniequynh@gmail.com>
2024-12-20 14:49:04 +0100

    Add project files.

---
+++ innkapslingDEMO/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * 1: Objekt med begge variablene
+ * 2: Trekke ut metoder og flytte inn i klassen
+ * 3: Innkapsliing
+ * 4: Endre implementasjon
+ */
+
+using innkapslingDEMO;
+
+var stats = new Stats();
+while(true)
+{
+    Console.WriteLine("Write a number (or blanked to exit)");
+    var numberStr = Console.ReadLine();
+    if (string.IsNullOrEmpty(numberStr)) break;
+    var number = Convert.ToInt32(numberStr);
+    stats.Numbers [stats.NumberCount++] = number;
+    stats.NumberCount++;
+    var sum = stats.Numbers.Sum();
+    Console.WriteLine(
+        $"Quantity of numbers: {stats.NumberCount} " +
+        $"Summary: {sum} " +
+        $"Average: {(float)sum/stats.NumberCount}");
+}
---
+++ innkapslingDEMO/Stats.cs
@@ -0,0 +1,13 @@
+﻿namespace innkapslingDEMO
+{
+    internal class Stats
+    {
+        public int[] Numbers;
+        public int NumberCount;
+
+        public Stats()
+        {
+            Numbers = new int [100];
+        }
+    }
+}
